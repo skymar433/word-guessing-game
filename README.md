7e63c951faa4aa22f25c60126b0c0f2caf1a14e5
Skylesha <skylesha@gmail.com>
2025-09-05 19:55:57 -0700

    feat: initial commit of word guessing game

---
+++ README.md
@@ -0,0 +1,13 @@
+# Word Guessing Game 🎯
+
+A simple Python word guessing game built in Visual Studio Code.
+
+## 📌 Features
+- Randomly picks a word from a word bank  
+- User guesses one letter at a time  
+- Tracks remaining attempts  
+- Tells you when you win or lose  
+
+## ▶️ How to Run
+```bash
+py "Word Guessing Game - Python.py"
---
+++ Word Guessing Game - Python.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+word_bank = ['sky', 'dyanne', 'rebecca', 'lala', 'gavin']
+
+word = random.choice(word_bank)
+guessedWord = ['_'] * len(word)
+
+attempts = 20
+
+while attempts > 0:
+  print('\nCurrent word: ' + ' '.join(guessedWord))
+
+  guess = input('Guess a letter: ').lower()
+   
+  if guess in word:
+    for i in range(len(word)):
+        if word[i] == guess:
+            guessedWord[i] = guess
+    print('Great guess!')
+  else:
+    attempts -= 1
+    print('Wrong guess! Attempts left: ' + str(attempts))
+  if '_' not in guessedWord:
+    print('\nCongratulations!! You guessed the word: ' + word)
+    break
+
+if attempts == 0 and '_' in guessedWord:
+  print('\nYou\'ve run out of attempts! The word was: ' + word)
